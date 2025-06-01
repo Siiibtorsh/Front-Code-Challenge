c587b2b274d0430b9dce40cb1dfd2337775f2bc1
sina-sahraiann <sinasahraeian780@gmail.com>
2025-06-02 01:03:14 +0330

    fixed issues

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,6 +2,6 @@ import ApiClient from './api';
 
 export const fetchUsers = async (page, limit) => {
    const response = await ApiClient.get(`users?_page=${page}&_limit=${limit}`);
-   await new Promise(resolve => setTimeout(resolve, 1000)); // Keeping the 1s delay
+   await new Promise(resolve => setTimeout(resolve, 1000));
    return response;
 };
